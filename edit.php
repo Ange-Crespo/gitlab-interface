4d6a886dfdd6df81b03c2f99b1df8f2b96e35edd
Ange <ange.crespo@etu.emse.fr>
2016-08-03 16:29:18 +0200

    oublie dans le précèdent commit

---
+++ edit.php
@@ -0,0 +1,83 @@
+<div>
+	
+	<table class="table no-border">
+
+		<tr>
+			<td>Titre: </td>
+			<td><input type="text" id="Title" maxlength="45" required/></td>
+		</tr>
+
+		<tr>
+			<td>Description:</td>
+			<td><textarea type="text" id="description_Edit" /></td>
+		</tr>
+
+		<tr>
+			<td>Assignee</td>
+			<td><input type="text" maxlength="15" id="assignee" list="users">
+
+				<datalist id="users">
+
+					<option>Chlore</option>
+
+					<option>PH</option>
+
+					<option>TAC</option>
+
+					<option>Chlore Choc</option>
+
+															
+				</datalist>
+
+			</td>
+		</tr>
+		
+		<tr>
+			<td>Module</td>
+			<td><input type="text" maxlength="15" id="module" list="modules">
+
+				<datalist id="modules">
+
+					<option>Chlore</option>
+
+					<option>PH</option>
+
+					<option>TAC</option>
+
+					<option>Chlore Choc</option>
+
+															
+				</datalist>
+
+			</td>
+		</tr>
+
+		<tr>
+			<td>Version détectée</td>
+			<td><input type="text" maxlength="15" id="version_detectee"></td>
+		
+			<td>Version résolue</td>
+			<td><input type="text" maxlength="15" id="version_resolue"></td>
+		</tr>
+	
+		<tr>
+			<td>Type</td>
+			<td><input type="text" maxlength="15" id="type" list="types">
+				<datalist id="types">
+
+					<option>Bug/Issue</option>
+
+					<option>Amélioration</option>
+															
+				</datalist>
+			</td>
+		</tr>
+											
+		<tr>
+			<td colspan="2" style="text-align: center;">
+			<input type="submit" value="Editer"/></td>
+		</tr>
+
+	</table>
+
+</div>
